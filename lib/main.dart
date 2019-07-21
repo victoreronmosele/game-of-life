1c7c09f137b64056779fba970a11e539b3b4b770
herovickers <victore@wakanow.com>
2019-07-21 21:02:18 +0100

    Add ability to react to neighbor states

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
@@ -27,33 +28,125 @@ class GameOfLifePlayground extends StatefulWidget {
 }
 
 class _GameOfLifePlaygroundState extends State<GameOfLifePlayground> {
+  GlobalKey _customPaintKey = GlobalKey();
+  final double _screenPadding = 10.0;
+  final int _numberOfBoxRows = 50;
+  Size _customPaintSize;
+  List<Cell> _listOfCells = [];
+  double _boxHeightDimension;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback(_getCustomPaintSize);
+    WidgetsBinding.instance.addPostFrameCallback(getListOfCells);
+  }
+
   void _startGame() {
     print('_startGame');
-    // setState(() {});
+    // _checkForCellStates();
+    _checkForNeighboringCellStates();
+  }
+
+  _checkForNeighboringCellStates() {
+    _listOfCells.forEach((cell) {
+      int index = _listOfCells.indexOf(cell);
+
+      List<Offset> listOfNeighboringPoints =
+          cell.getNeighborPoints(_boxHeightDimension);
+
+      List<Cell> listOfNeigboringCells = _listOfCells.where((cell) {
+        return listOfNeighboringPoints.contains(cell.point);
+      }).toList();
+
+      _reactToNeighbors(cell, listOfNeigboringCells);
+    });
+  }
+
+  _reactToNeighbors(Cell cell, List<Cell> listOfNeighboringCells) {
+    int numberOfLivingNeighbors = listOfNeighboringCells
+        .where((_) => _.cellState == CellState.alive)
+        .length;
+    print(
+        'Cell initial Status ====> ${(cell.cellState).toString().toUpperCase()}');
+
+    print('Living neighbors are ====> $numberOfLivingNeighbors');
+
+    CellState newCellState = Rule().getNewCellState(
+        initialCellState: cell.cellState,
+        numberOfLivingNeighbors: numberOfLivingNeighbors);
+
+    cell.cellState = newCellState;
+    print('cellState = ${cell.cellState} ');
+
+    print('');
+  }
+
+  _checkForCellStates() {
+    _listOfCells.forEach((cell) {
+      int index = _listOfCells.indexOf(cell);
+      print(index.toString() + cell.cellState.toString());
+    });
+  }
+
+  void getListOfCells(_) {
+    //TODO Account for top and bottom padding to calculate height
+    setState(() {
+      _boxHeightDimension = _customPaintSize.height / _numberOfBoxRows;
+    });
+
+    final radius = _boxHeightDimension / 2;
+    final int numberOfBoxColumns =
+        (_customPaintSize.width - _screenPadding) ~/ _boxHeightDimension;
+
+    Offset offset = Offset(radius, radius);
+
+    for (var i = 1; i < _numberOfBoxRows; i++) {
+      offset = Offset(radius, i * _boxHeightDimension);
+
+      for (var i = 0; i < numberOfBoxColumns; i++) {
+        var newOffsetDx = offset.dx + _boxHeightDimension;
+        var newOffsetDy = offset.dy;
+        offset = Offset(newOffsetDx, newOffsetDy);
+        _listOfCells.add(Cell(
+            Random().nextInt(3).isEven ? CellState.alive : CellState.dead,
+            offset));
+      }
+    }
+
+    print(_listOfCells.length.toString() + 'cells');
+  }
+
+  _getCustomPaintSize(_) {
+    final RenderBox containerRenderBox =
+        _customPaintKey.currentContext.findRenderObject();
+    _customPaintSize = containerRenderBox.size;
+    print('_customPaintSize => $_customPaintSize');
   }
 
   @override
   Widget build(BuildContext context) {
-    print('main build');
-
-    double screenPadding = 10.0;
-
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.black,
         appBar: AppBar(
-          title: Text(widget.title),
+          title: Text(
+            widget.title,
+          ),
           backgroundColor: Colors.transparent,
           elevation: 0.0,
         ),
         body: Padding(
-          padding: EdgeInsets.all(screenPadding),
+          padding: EdgeInsets.all(_screenPadding),
           child: Container(
             color: Colors.black,
             child: CustomPaint(
+              key: _customPaintKey,
               painter: GameOfLifePainter(
-                padding: 2 * screenPadding,
-              ),
+                  padding: 2 * _screenPadding,
+                  numberOfBoxRows: _numberOfBoxRows,
+                  listOfCells: _listOfCells,
+                  boxHeightDimension: _boxHeightDimension),
               child: Container(),
             ),
           ),
@@ -72,69 +165,40 @@ class _GameOfLifePlaygroundState extends State<GameOfLifePlayground> {
 
 class GameOfLifePainter extends CustomPainter {
   final double padding;
+  final int numberOfBoxRows;
+  final List<Cell> listOfCells;
+  final boxHeightDimension;
 
-  GameOfLifePainter({
-    @required this.padding,
-  });
+  GameOfLifePainter(
+      {@required this.padding,
+      @required this.numberOfBoxRows,
+      @required this.listOfCells,
+      @required this.boxHeightDimension});
 
   @override
   void paint(Canvas canvas, Size size) {
-    print('width size is ${size.width}');
+    drawGrid(listOfCells, boxHeightDimension, canvas);
+  }
 
-    final int numberOfBoxRows = 50;
-    //TODO Account for top and bottom padding to calculate height
-    final boxHeightDimension = size.height / numberOfBoxRows;
-    final radius = boxHeightDimension / 2;
-    final int numberOfBoxColumns = (size.width - padding) ~/ boxHeightDimension;
-
-    print('number of box columns $numberOfBoxColumns');
-
-    List<Cell> listOfCells = [];
-
-    Offset offset = Offset(radius, radius);
-
-    for (var i = 1; i < numberOfBoxRows; i++) {
-      offset = Offset(radius, i * boxHeightDimension);
-
-      for (var i = 0; i < numberOfBoxColumns; i++) {
-        print(offset);
-        var newOffsetDx = offset.dx + boxHeightDimension;
-        var newOffsetDy = offset.dy;
-        offset = Offset(newOffsetDx, newOffsetDy);
-        listOfCells.add(Cell(
-            Random().nextInt(3).isEven ? CellState.alive : CellState.dead,
-            offset));
-      }
-    }
-
+  void drawGrid(
+      List<Cell> listOfCells, double boxHeightDimension, Canvas canvas) {
     for (var i = 0; i < listOfCells.length; i++) {
+      Cell currentCell = listOfCells.elementAt(i);
       final paint = Paint()
         ..color = Colors.blue
-        ..style = listOfCells.elementAt(i).cellState == CellState.alive
+        ..style = currentCell.cellState == CellState.alive
             ? PaintingStyle.fill
             : PaintingStyle.stroke;
 
-      if (i + 1 >= listOfCells.length) {
-        break;
-      } else {
-        Offset firstBoxPoint = listOfCells.elementAt(i).point;
-        Offset secondBoxPoint = listOfCells.elementAt(i + 1).point;
-
-        if (firstBoxPoint == secondBoxPoint) continue;
-
-// canvas.
-        canvas.drawRect(
-            Rect.fromCircle(
-              center: firstBoxPoint,
-              radius: boxHeightDimension - padding,
-            ),
-            paint);
-      }
+      Offset centerPoint = currentCell.point;
+      Rect rect = Rect.fromCircle(
+          center: centerPoint, radius: boxHeightDimension - padding);
+      canvas.drawRect(rect, paint);
     }
   }
 
   @override
-  bool shouldRepaint(GameOfLifePainter oldDelegate) => false;
+  bool shouldRepaint(GameOfLifePainter oldDelegate) => true;
 }
 
 enum CellState { alive, dead }
@@ -165,5 +229,23 @@ class Cell {
     ];
 
     return neighborPoints;
+  }
+}
+
+class Rule {
+  CellState getNewCellState(
+      {@required CellState initialCellState,
+      @required int numberOfLivingNeighbors}) {
+    CellState newCellState;
+
+    if (numberOfLivingNeighbors == 3) {
+      newCellState = CellState.alive;
+    } else if (numberOfLivingNeighbors != 2) {
+      newCellState = CellState.dead;
+    } else {
+      newCellState = initialCellState;
+    }
+
+    return newCellState;
   }
 }
